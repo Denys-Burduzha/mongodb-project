b9f256c5a8b5ce5ed0a1e36ad24193eb5fad274c
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2023-03-09 13:47:15 +0100

    apdate

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-//Fedir4lapy1hvost,
-
-// mongodb+srv://Denys:<password>@cluster0.p4dr4y4.mongodb.net/test
-
-const mogoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const {DB_HOST} = process.env;
 
 
-mogoose.connect(DB_HOST)
-    .then(() => console.log("Database connect"))
-    .catch(error => {
-        console.log(error.message);
-        process.exit(1);
-});
+// const mogoose = require("mongoose");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const {DB_HOST} = process.env;
+
+
+// mogoose.connect(DB_HOST)
+//     .then(() => console.log("Database connect"))
+//     .catch(error => {
+//         console.log(error.message);
+//         process.exit(1);
+// });
